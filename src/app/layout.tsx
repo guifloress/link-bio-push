4ecbf0ff892f60001af2b237227d12339d36ed52
Guilherme Florentino <florentinoo.guilherme@gmail.com>
2026-02-12 22:31:28 +0000

    utilize o arquivo favicon.svg que esta em src/images.
    Este arquivo deve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'BioLink - Guilherme Florentino',
   description: 'Web Designer Portfolio and Links',
+  icons: {
+    icon: '/bio/favicon.svg',
+  },
 };
 
 export default function RootLayout({
